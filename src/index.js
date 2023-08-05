2bd9391b52450c1493789245457f49a927a8d934
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-06 00:32:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/project-FunDevs6">
+    <BrowserRouter basename="/react_node_fs_67_OlenaNov">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
